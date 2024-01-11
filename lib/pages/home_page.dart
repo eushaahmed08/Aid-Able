bbab0e05dcdaeab7ad78bba092569095ce938d41
khalidRatin <153265332+khalidRatin@users.noreply.github.com>
2024-01-11 11:23:37 +0600

    drawer created

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,55 +19,61 @@ class _HomePageState extends State<HomePage> {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('AidAble'),
-          leading: IconButton(
-            icon: const Icon(Icons.density_medium),
-            onPressed: () {},
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.density_medium),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              );
+            },
           ),
         ),
         body: _selectedIndex == 0
             ? ListView.builder(
-                itemCount: 5,
-                itemBuilder: (BuildContext context, int index) {
-                  String itemName = '';
-                  switch (index) {
-                    case 0:
-                      itemName = 'Food';
-                      break;
-                    case 1:
-                      itemName = 'Clothes';
-                      break;
-                    case 2:
-                      itemName = 'Books';
-                      break;
-                    case 3:
-                      itemName = 'Blankets';
-                      break;
-                    case 4:
-                      itemName = 'Toys';
-                      break;
-                    default:
-                      itemName = 'Unknown';
-                  }
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            String itemName = '';
+            switch (index) {
+              case 0:
+                itemName = 'Food';
+                break;
+              case 1:
+                itemName = 'Clothes';
+                break;
+              case 2:
+                itemName = 'Books';
+                break;
+              case 3:
+                itemName = 'Blankets';
+                break;
+              case 4:
+                itemName = 'Toys';
+                break;
+              default:
+                itemName = 'Unknown';
+            }
 
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 56.6929,
-                      decoration: BoxDecoration(
-                        color: Colors.purple,
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: Text(
-                          itemName,
-                          style: const TextStyle(
-                              fontSize: 20.0, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              )
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 56.6929,
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Center(
+                  child: Text(
+                    itemName,
+                    style: const TextStyle(
+                        fontSize: 20.0, color: Colors.white),
+                  ),
+                ),
+              ),
+            );
+          },
+        )
             : Container(),
         bottomNavigationBar: BottomAppBar(
           child: Row(
@@ -76,9 +82,10 @@ class _HomePageState extends State<HomePage> {
               ElevatedButton(
                 style: ButtonStyle(
                   elevation: MaterialStateProperty.resolveWith<double>(
-                      (Set<MaterialState> states) {
-                    return _selectedIndex == 0 ? 8.0 : 0.0;
-                  }),
+                        (Set<MaterialState> states) {
+                      return _selectedIndex == 0 ? 8.0 : 0.0;
+                    },
+                  ),
                 ),
                 onPressed: () {
                   _onItemTapped(0);
@@ -88,16 +95,18 @@ class _HomePageState extends State<HomePage> {
               ElevatedButton(
                 style: ButtonStyle(
                   elevation: MaterialStateProperty.resolveWith<double>(
-                      (Set<MaterialState> states) {
-                    return _selectedIndex == 1 ? 8.0 : 0.0;
-                  }),
+                        (Set<MaterialState> states) {
+                      return _selectedIndex == 1 ? 8.0 : 0.0;
+                    },
+                  ),
                 ),
                 onPressed: () {
                   _onItemTapped(1);
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const AddPostPage()),
+                      builder: (context) => const AddPostPage(),
+                    ),
                   );
                 },
                 child: const Icon(Icons.post_add),
@@ -105,9 +114,10 @@ class _HomePageState extends State<HomePage> {
               ElevatedButton(
                 style: ButtonStyle(
                   elevation: MaterialStateProperty.resolveWith<double>(
-                      (Set<MaterialState> states) {
-                    return _selectedIndex == 2 ? 8.0 : 0.0;
-                  }),
+                        (Set<MaterialState> states) {
+                      return _selectedIndex == 2 ? 8.0 : 0.0;
+                    },
+                  ),
                 ),
                 onPressed: () {
                   _onItemTapped(2);
@@ -115,6 +125,39 @@ class _HomePageState extends State<HomePage> {
                 child: const Icon(Icons.question_answer),
               ),
             ],
+          ),
+        ),
+        drawer: Drawer(
+          child: Container(
+            width: MediaQuery.of(context).size.width / 2,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text("Your App Name"),
+                  accountEmail: Text(user.email ?? ""),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.person),
+                  ),
+                ),
+                ListTile(
+                  title: Text("Drawer Item 1"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    // Handle drawer item tap
+                  },
+                ),
+                ListTile(
+                  title: Text("Drawer Item 2"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    // Handle drawer item tap
+                  },
+                ),
+                // Add more ListTile widgets for additional drawer items
+              ],
+            ),
           ),
         ),
       ),
@@ -126,48 +169,4 @@ class _HomePageState extends State<HomePage> {
       _selectedIndex = index;
     });
   }
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedIndex = 0;
-  }
-
-  // Method to navigate to the second page
-  void navigateToSecondPage(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (context) =>
-              const HomePage()), // Using the second code's page
-    );
-  }
-
-
-  // Sign user out method
-  void signUserOut() {
-    FirebaseAuth.instance.signOut();
-  }
-
-  // //  @override
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       actions: [
-  //         IconButton(
-  //           onPressed: () => navigateToSecondPage(context), // Navigate when icon is pressed
-  //           icon: const Icon(Icons.logout),
-  //         )
-  //       ],
-  //     ),
-  //     body: Center(
-  //       child: Text(
-  //         "Logged In As ${user.email!}",
-  //         style: const TextStyle(fontSize: 20),
-  //       ),
-  //     ),
-  //   );
-  // }
 }
-
