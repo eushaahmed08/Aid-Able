8c359fb412cd10a444cc7a0e9f1c72a4b1d52a72
eushaahmed08 <eushaahmed08@gmail.com>
2024-01-11 10:46:35 +0600

    added more

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:aid_able/pages/addpost.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'r.dart'; // Import the file containing the second code
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key});
@@ -11,71 +10,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   int _selectedIndex = 0;
   final user = FirebaseAuth.instance.currentUser!;
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-
           title: const Text('AidAble'),
           leading: IconButton(
             icon: const Icon(Icons.density_medium),
-
             onPressed: () {},
           ),
         ),
         body: _selectedIndex == 0
             ? ListView.builder(
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) {
-            String itemName = '';
-            switch (index) {
-              case 0:
-                itemName = 'Food';
-                break;
-              case 1:
-                itemName = 'Clothes';
-                break;
-              case 2:
-                itemName = 'Books';
-                break;
-              case 3:
-                itemName = 'Blankets';
-                break;
-              case 4:
-                itemName = 'Toys';
-                break;
-              default:
-                itemName = 'Unknown';
-            }
+                itemCount: 5,
+                itemBuilder: (BuildContext context, int index) {
+                  String itemName = '';
+                  switch (index) {
+                    case 0:
+                      itemName = 'Food';
+                      break;
+                    case 1:
+                      itemName = 'Clothes';
+                      break;
+                    case 2:
+                      itemName = 'Books';
+                      break;
+                    case 3:
+                      itemName = 'Blankets';
+                      break;
+                    case 4:
+                      itemName = 'Toys';
+                      break;
+                    default:
+                      itemName = 'Unknown';
+                  }
 
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 56.6929,
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Center(
-                  child: Text(
-                    itemName,
-
-                    style: const TextStyle(fontSize: 20.0, color: Colors.white),
-
-                  ),
-                ),
-              ),
-            );
-          },
-        )
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 56.6929,
+                      decoration: BoxDecoration(
+                        color: Colors.purple,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          itemName,
+                          style: const TextStyle(
+                              fontSize: 20.0, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              )
             : Container(),
         bottomNavigationBar: BottomAppBar(
           child: Row(
@@ -83,20 +75,20 @@ class _HomePageState extends State<HomePage> {
             children: <Widget>[
               ElevatedButton(
                 style: ButtonStyle(
-                  elevation: MaterialStateProperty.resolveWith<double>((Set<MaterialState> states) {
+                  elevation: MaterialStateProperty.resolveWith<double>(
+                      (Set<MaterialState> states) {
                     return _selectedIndex == 0 ? 8.0 : 0.0;
                   }),
                 ),
                 onPressed: () {
                   _onItemTapped(0);
                 },
-
                 child: const Icon(Icons.feed),
-
               ),
               ElevatedButton(
                 style: ButtonStyle(
-                  elevation: MaterialStateProperty.resolveWith<double>((Set<MaterialState> states) {
+                  elevation: MaterialStateProperty.resolveWith<double>(
+                      (Set<MaterialState> states) {
                     return _selectedIndex == 1 ? 8.0 : 0.0;
                   }),
                 ),
@@ -104,15 +96,16 @@ class _HomePageState extends State<HomePage> {
                   _onItemTapped(1);
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const AddPostPage()),
+                    MaterialPageRoute(
+                        builder: (context) => const AddPostPage()),
                   );
                 },
                 child: const Icon(Icons.post_add),
-
               ),
               ElevatedButton(
                 style: ButtonStyle(
-                  elevation: MaterialStateProperty.resolveWith<double>((Set<MaterialState> states) {
+                  elevation: MaterialStateProperty.resolveWith<double>(
+                      (Set<MaterialState> states) {
                     return _selectedIndex == 2 ? 8.0 : 0.0;
                   }),
                 ),
@@ -120,7 +113,6 @@ class _HomePageState extends State<HomePage> {
                   _onItemTapped(2);
                 },
                 child: const Icon(Icons.question_answer),
-
               ),
             ],
           ),
@@ -145,7 +137,9 @@ class _HomePageState extends State<HomePage> {
   void navigateToSecondPage(BuildContext context) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => const MyApp()), // Using the second code's page
+      MaterialPageRoute(
+          builder: (context) =>
+              const HomePage()), // Using the second code's page
     );
   }
 
