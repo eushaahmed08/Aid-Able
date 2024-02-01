9bcd08cfad4c788b5b48e49a10527b6f7dbd48cd
eushaahmed08 <eushaahmed08@gmail.com>
2024-02-01 12:28:00 +0600

    notif has bugs

--- lib/pages/notif.dart
+++ lib/pages/notif.dart
@@ -19,3 +19,26 @@ class PushNotifications {
     final token = await _firebaseMessaging.getToken();
     print("device token: $token");
   }
+
+
+  // initalize local notifications
+  static Future localNotiInit() async {
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+    final DarwinInitializationSettings initializationSettingsDarwin =
+    DarwinInitializationSettings(
+      onDidReceiveLocalNotification: (id, title, body, payload) => null,
+    );
+    final LinuxInitializationSettings initializationSettingsLinux =
+    LinuxInitializationSettings(defaultActionName: 'Open notification');
+    final InitializationSettings initializationSettings =
+    InitializationSettings(
+        android: initializationSettingsAndroid,
+        iOS: initializationSettingsDarwin,
+        linux: initializationSettingsLinux);
+    _flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: onNotificationTap,
+        onDidReceiveBackgroundNotificationResponse: onNotificationTap);
+  }
+
